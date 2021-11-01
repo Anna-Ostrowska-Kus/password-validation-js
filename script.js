d2339118688a87e4a45419d465efdea6e59d2309
Anna Ostrowska-Kus <anna.ostrowska@wp.pl>
2021-11-01 20:37:56 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const pass = document.getElementById('password');
+const p = document.querySelector('.passinfo');
+const letters = /[a-z]/i;
+const numbers = /[0-9]/;
+const special = /[!@#$%^&*()]/;
+const minValue = 10;
+
+
+
+const showInfo =()=>{
+    console.log(pass.value);
+    if (pass.value.length>minValue && letters.test(pass.value) && numbers.test(pass.value) && special.test(pass.value) ){
+        p.textContent = "Very strong password"
+
+    } else if( pass.value.length>minValue && letters.test(pass.value) && numbers.test(pass.value)){
+        p.textContent = "Strong password"
+    } else if(pass.value === ""){
+        p.textContent = "Nie podałeś hasła..."
+    }
+    else{
+    
+        p.textContent= "Weak password"
+    }
+}
+
+pass.addEventListener('keyup', function(e){
+    if (e.keyCode === 13) {
+        showInfo()
+        
+    }
+})
+// pass.addEventListener('input', showInfo)
+
+
+
+
+
