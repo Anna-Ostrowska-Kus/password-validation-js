90015858ab712c470b9c4c8e4d33286e42d749ac
Anna Ostrowska-Kus <anna.ostrowska@wp.pl>
2021-11-02 18:57:17 +0100

    add colors

--- script.js
+++ script.js
@@ -11,20 +11,24 @@ const showInfo =()=>{
     
     if (pass.value.length>minValue && letters.test(pass.value) && numbers.test(pass.value) && special.test(pass.value) ){
         p.textContent = "Bardzo silne hasło"
+        p.style.color = "lime"
 
     } else if( pass.value.length>minValue && letters.test(pass.value) && numbers.test(pass.value)){
         p.textContent = "Silne hasło"
+        p.style.color = "yellow"
     } else if(pass.value === ""){
         p.textContent = "Nie podałeś hasła..."
     }
     else{
     
         p.textContent= "Słabe hasło"
+        p.style.color = "tomato"
     }
 }
 
 
 pass.addEventListener('input', showInfo)
+
 
 
 
